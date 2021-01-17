98fe83f3e03026891846f2465f56fc3caaa24087
Kyle Lin <kylejlin.us@gmail.com>
2021-01-17 15:47:19 -0800

    Remove unused config code

--- particles/src/particles.ts
+++ particles/src/particles.ts
@@ -8,7 +8,6 @@ export function startAnimationLoop(
 
   const WIDTH = canvas.width;
   const HEIGHT = canvas.height;
-  const config = getConfig();
 
   const ctx = canvas.getContext("2d")!;
   const emitters: Emitter[] = addedEmitters.map(fromSpec);
@@ -120,10 +119,6 @@ export function startAnimationLoop(
       emitters.pop();
     }
   }
-}
-
-function getConfig() {
-  return {} as const;
 }
 
 export interface Particle {
